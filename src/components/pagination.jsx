1b9b93c6fc6172f1bfd9ae77361d4ef9a19def26
huseyinozell <hseyinzel@hotmail.com>
2024-05-28 13:23:46 +0300

    Search Bar Implemented

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,4 +1,3 @@
-// src/components/Pagination.js
 import React, {useEffect, useState} from 'react';
 import {
   View,
@@ -7,15 +6,18 @@ import {
   StyleSheet,
   ActivityIndicator,
   TouchableOpacity,
+  TextInput,
 } from 'react-native';
 
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'; // Import icon library
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {fetchData} from '../services/rickAndMortyService';
 
 const PaginationComponent = ({renderItem, url}) => {
-  const [data, setData] = useState([]);
+  const [originalData, setOriginalData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
   const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(false);
+  const [searchText, setSearchText] = useState('');
 
   const renderItemFunction = ({item}) => renderItem({item});
 
@@ -23,17 +25,16 @@ const PaginationComponent = ({renderItem, url}) => {
     setLoading(true);
 
     const itemsPerPage = 5;
-
     const start = (pageNumber - 1) * itemsPerPage + 1;
     const idList = Array.from({length: itemsPerPage}, (_, i) => i + start).join(
       ',',
     );
-
     const data = await fetchData(idList, url);
 
     if (data) {
       const dataList = Array.isArray(data) ? data : [data];
-      setData(dataList);
+      setOriginalData(dataList);
+      setFilteredData(dataList);
       setPage(pageNumber);
     }
 
@@ -50,6 +51,22 @@ const PaginationComponent = ({renderItem, url}) => {
     }
   };
 
+  const handleSearch = text => {
+    setSearchText(text);
+    const filtered = filterData(text);
+    setFilteredData(filtered);
+  };
+
+  const filterData = searchText => {
+    return originalData.filter(
+      item =>
+        item.name.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.status?.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.species?.toLowerCase().includes(searchText.toLowerCase()) ||
+        item.gender?.toLowerCase().includes(searchText.toLowerCase()),
+    );
+  };
+
   const renderFooter = () => {
     if (!loading) return null;
     return <ActivityIndicator style={{margin: 20}} />;
@@ -57,8 +74,15 @@ const PaginationComponent = ({renderItem, url}) => {
 
   return (
     <View style={styles.container}>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Arama.."
+        placeholderTextColor={'black'}
+        onChangeText={handleSearch}
+        value={searchText}
+      />
       <FlatList
-        data={data}
+        data={filteredData}
         keyExtractor={item => item.id.toString()}
         renderItem={renderItemFunction}
         ListFooterComponent={renderFooter}
@@ -89,6 +113,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    padding: 5,
+    color: 'black',
   },
   pagination: {
     flexDirection: 'row',
