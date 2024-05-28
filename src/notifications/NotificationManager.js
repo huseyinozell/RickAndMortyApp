e3d935f86ab7247178e179eab48f5e844b42ca83
huseyinozell <hseyinzel@hotmail.com>
2024-05-28 12:47:17 +0300

    Refactoring

--- src/notifications/NotificationManager.js
+++ src/notifications/NotificationManager.js
@@ -5,33 +5,30 @@ class NotificationManager {
     constructor(){
         PushNotification.createChannel(
             {
-              channelId: "local-channel", // (required)
-              channelName: "My channel", // (required)
-              channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
-              playSound: false, // (optional) default: true
-              soundName: "default", // (optional) See `soundName` parameter of `localNotification` function
-              importance: 4, // (optional) default: 4. Int value of the Android notification importance
-              vibrate: true, // (optional) default: true. Creates the default vibration patten if true.
+              channelId: "local-channel", 
+              channelName: "Rick And Morty Channel", 
+              channelDescription: "Rick and Morty Notification Channel", 
+              playSound: false, 
+              soundName: "default", 
+              importance: 4, 
+              vibrate: true, 
             },
-            (created) => console.log(`createChannel returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
+            (created) => console.log(`Channel create returned '${created}'`) // (optional) callback returns whether the channel was created, false means it already existed.
           );
     }
 
 
   configure = () => {
     PushNotification.configure({
-      // (optional) Called when Token is generated (iOS and Android)
+      
       onRegister: function (token) {
         console.log("TOKEN:", token);
       },
 
-      // (required) Called when a remote is received or opened, or local notification is opened
+      
       onNotification: function (notification) {
         console.log("NOTIFICATION:", notification);
 
-        // process the notification
-
-        // (required) Called when a remote is received or opened, or local notification is opened
         notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
 
@@ -40,18 +37,18 @@ class NotificationManager {
         console.log("ACTION:", notification.action);
         console.log("NOTIFICATION:", notification);
 
-        // process the action
+        
       },
 
-      // (optional) Called when Notification is clicked (Android)
+      
       onNotificationOpened: function (notification) {
         console.log("NOTIFICATION OPENED:", notification);
       },
 
-      // ANDROID ONLY: GCM Sender ID (optional - not required for local notifications, but is need to receive remote push notifications)
+      
       senderID: "YOUR GCM SENDER ID",
 
-      // IOS ONLY (optional): default: all - Permissions to register.
+      
       permissions: {
         alert: true,
         badge: true,
