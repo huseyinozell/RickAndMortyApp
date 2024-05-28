855cb14d8ff5934e2b43a047bc3a19fb758426c7
huseyinozell <hseyinzel@hotmail.com>
2024-05-28 11:36:13 +0300

    Favorite Icon Added

--- App.jsx
+++ App.jsx
@@ -37,7 +37,7 @@ const MainTabNavigator = () => {
         component={FavoriteCharacterList}
         options={{
           tabBarIcon: ({color, size}) => (
-            <Icon name="basketball" color={color} size={size} />
+            <Icon name="star" color={color} size={size} />
           ),
         }}
       />
